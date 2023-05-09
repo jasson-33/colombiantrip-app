0bda209a486734a1d9ac40a4af6fba00d430d42f
Javier Hdz <54322254+chavi2503@users.noreply.github.com>
2023-05-09 12:33:23 -0500

    Font Context

--- src/context/ColombianContext.jsx
+++ src/context/ColombianContext.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import localFont from 'next/font/local';
 const Bigola = localFont({ src: '../styles/fonts/Bigola-Display-Regular.ttf' });
 const Gotham = localFont({ src: '../styles/fonts/Gotham-Book.otf' });
+const GothamBold = localFont({ src: '../styles/fonts/Gotham-Bold.otf' });
 export const ColombianContext = React.createContext({});
 
 const ColombianContextProvider = ({ children }) => {
   return (
-    <ColombianContext.Provider value={{ Bigola, Gotham }}>
+    <ColombianContext.Provider value={{ Bigola, Gotham, GothamBold }}>
       {children}
     </ColombianContext.Provider>
   );
