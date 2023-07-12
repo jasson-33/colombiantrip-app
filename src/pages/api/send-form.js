c6e10a28efc185b71a9c687df4507c64abc58517
AngelloPaperplane <dev@paperplane.co>
2023-07-12 12:57:20 -0500

    fix lint

--- src/pages/api/send-form.js
+++ src/pages/api/send-form.js
@@ -1,27 +1,29 @@
 export default function handler(req, res) {
-    const FormData = require('form-data');
-    const body = req.body;
-    console.log('body: ', body);
+  const FormData = require('form-data');
+  const body = req.body;
+  console.log('body: ', body);
 
-    const formData = new FormData();
-    formData.append('first_name', body.first_name);
-    formData.append('last_name', body.last_name);
-    formData.append('email', body.email);
-    formData.append('duration', body.duration);
-    formData.append('number_travelers', body.number_travelers);
-    formData.append('date_month', body.date_month);
-    formData.append('date_day', body.date_day);
-    formData.append('date_year', body.date_year);
-    formData.append('style_travel', body.style_travel);
-    formData.append('style_accomodation', body.style_accomodation);
-    formData.append('style_destination', body.style_destination);
-    formData.append('message', body.message);
-    const options = {
-        method: 'POST',
-        body: formData,
-      };
-    fetch(`${process.env.NEXT_PUBLIC_ENDPOINT_CONTENT}colombian-app/v2/contact-footer-full`, options)
-      .then((response) => response.json())
-      .then((data) => res.status(200).json(data));
-      
-  }
+  const formData = new FormData();
+  formData.append('first_name', body.first_name);
+  formData.append('last_name', body.last_name);
+  formData.append('email', body.email);
+  formData.append('duration', body.duration);
+  formData.append('number_travelers', body.number_travelers);
+  formData.append('date_month', body.date_month);
+  formData.append('date_day', body.date_day);
+  formData.append('date_year', body.date_year);
+  formData.append('style_travel', body.style_travel);
+  formData.append('style_accomodation', body.style_accomodation);
+  formData.append('style_destination', body.style_destination);
+  formData.append('message', body.message);
+  const options = {
+    method: 'POST',
+    body: formData,
+  };
+  fetch(
+    `${process.env.NEXT_PUBLIC_ENDPOINT_CONTENT}colombian-app/v2/contact-footer-full`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => res.status(200).json(data));
+}
