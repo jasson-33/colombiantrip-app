b2684f0388fc0f1bf46ac90fa3d7ba1be01eb68e
AngelloPaperplane <dev@paperplane.co>
2023-08-07 21:30:51 -0500

    lint

--- src/components/legalContent/index.jsx
+++ src/components/legalContent/index.jsx
@@ -3,10 +3,13 @@ import React, { useContext } from 'react';
 import styles from './legalContent.module.css';
 
 const LegalContent = ({ legalcontent }) => {
-    const { Bigola } = useContext(ColombianContext);
-    return (
-      <div className={`${Bigola.className}`} dangerouslySetInnerHTML={{__html:legalcontent}} />
-    );
-  };
-  
-  export default LegalContent;
+  const { Bigola } = useContext(ColombianContext);
+  return (
+    <div
+      className={`${Bigola.className}`}
+      dangerouslySetInnerHTML={{ __html: legalcontent }}
+    />
+  );
+};
+
+export default LegalContent;
