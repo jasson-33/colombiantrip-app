60912866f1804db7ac0cf457ea6d82d98c56a5aa
Crish777 <cristhianelden@gmail.com>
2023-08-31 09:11:16 -0500

    404

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -25,7 +25,6 @@ const Layout = ({ children }) => {
     }
   }, [animate]);
 
-  console.log(children.props)
 
   if (!children.props.data) {
     return <main className={`siteMain ${Gotham.className}`}>{children}</main>;
