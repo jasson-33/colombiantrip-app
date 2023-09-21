9a76830fcfafc7b5e5331f970ebb5a6c8759d421
AngelloPaperplane <dev@paperplane.co>
2023-09-21 10:19:01 -0500

    Add hide option to author and date

--- src/components/blog-after-hero/index.jsx
+++ src/components/blog-after-hero/index.jsx
@@ -9,19 +9,18 @@ const BlogArterfHero = ({ blogAfterHero }) => {
     <section className="siteSection">
       <div className={styles.blogAfterHero}>
         <div className={styles.contentDateBlog}>
-          <p className={styles.contAutorBlog}>
-            Written by{' '}
-            <span className={`${styles.autorBlog} ${Gotham_Bold.className}`}>
-              {blogAfterHero.autor}
-            </span>
-          </p>
-          <p className={styles.contDateBlog}>{blogAfterHero.date}</p>
+          {!blogAfterHero.hide_autor && (
+            <p className={styles.contAutorBlog}>
+              Written by{' '}
+              <span className={`${styles.autorBlog} ${Gotham_Bold.className}`}>
+                {blogAfterHero.autor}
+              </span>
+            </p>
+          )}
+          {!blogAfterHero.hide_date && (
+            <p className={styles.contDateBlog}>{blogAfterHero.date}</p>
+          )}
         </div>
-        {/*
-        <div className={`${styles.innerContSubtitle} ${Gotham_Bold.className}`}>
-          {blogAfterHero.subtitle}
-        </div>
-        */}
       </div>
     </section>
   );
--- src/pages/meet-colombia/detail/[id].jsx
+++ src/pages/meet-colombia/detail/[id].jsx
@@ -7,47 +7,23 @@ import React from 'react';
 
 const DetailPost = ({ data }) => {
   console.log(data);
-  const { metacontent, hero, blocks, dateformat, autor } = data;
+  const {
+    metacontent,
+    hero,
+    blocks,
+    dateformat,
+    autor,
+    hide_autor,
+    hide_date,
+  } = data;
 
   const blogAfterHero = {
     autor,
+    hide_autor,
+    hide_date,
     date: dateformat,
-    subtitle:
-      'Once tagged as one of the most dangerous cities in the world, Medellín has transformed and become a popular destination. Heres’ why you should visit Medellín.',
+    subtitle: '',
   };
-
-  /*
-  const getimgsBlock = {
-    imagesBlock: [
-      {
-        img: '/images/experiences/9.jpg',
-        alt: 'img-blog',
-      },
-      {
-        img: '/images/experiences/9.jpg',
-        alt: 'img-blog',
-      },
-    ],
-  };
-  const getimgsBlockFull = {
-    imagesBlock: [
-      {
-        img: '/images/experiences/9.jpg',
-        alt: 'img-blog',
-      },
-    ],
-  };
-
-  const textBlogDesc = {
-    title: '1. The weather in Medellín is amazing.',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  };
-
-  const textBlogDesctwo = {
-    title: '',
-    desc: 'Viverra ipsum nunc aliquet bibendum enim. Urna cursus eget nunc scelerisque viverra mauris. In ante metus dictum at tempor commodo ullamcorper a lacus. Vitae tortor condimentum lacinia quis. Sed cras ornare arcu dui vivamus. Nunc sed blandit libero volutpat sed cras ornare. Placerat in egestas erat imperdiet sed euismod nisi porta lorem.',
-  };
-  */
 
   function get_block_by_layout(block, index) {
     switch (block.layout) {
