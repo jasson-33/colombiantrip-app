d6bd4d73a4d143c47ca806e984af087035dbf4f0
Crish777 <cristhianelden@gmail.com>
2023-10-18 15:32:57 -0500

    [Fix] links

--- src/components/experience-detail/index.jsx
+++ src/components/experience-detail/index.jsx
@@ -22,38 +22,30 @@ const ExperienceDetail = ({ contentExp }) => {
     <section className={`siteSection ${styles.experienceDetailSection}`}>
       <div
         className={`container flex f-jc f-as ${styles.containerExperiencesDetail}`}>
-        {contentExp.map(({ img, width, title, id, external_link }, i) => (
-          <div
-            data-aos="fade-up"
-            data-aos-duration="900"
-            data-aos-delay="0"
-            key={id + i}
-            className={`${styles.cardExperience} ${styles[`experience-${i}`]}`}
-            style={{
-              width: width === '100%' ? width : `calc(${width} - 20px)`,
-            }}>
-            <Atropos className={styles.atroposExperience} {...atroposProps}>
-              {external_link && external_link !== '' ? (
-                <a href={external_link} target="_blank">
-                  <div
-                    className={`bg-cv ${styles.imgCardExperience}`}
-                    style={{
-                      backgroundImage: `url(${
-                        img.sizes
-                          ? width === '100%'
-                            ? img.url
-                            : img.sizes.large
-                          : ''
-                      })`,
-                    }}></div>
-                  <h2
-                    data-atropos-offset={`${width === '100%' ? '0' : '0.255'}`}
-                    className={`${styles.titleCard} ${Bigola.className}`}
-                    dangerouslySetInnerHTML={{ __html: title }}
-                  />
-                </a>
-              ) : (
-                <Link href={`${process.env.NEXT_PUBLIC_CURR_DOMAIN}${id}`}>
+        {contentExp.map(
+          ({ img, width, title, id, external_link, target }, i) => (
+            <div
+              data-aos="fade-up"
+              data-aos-duration="900"
+              data-aos-delay="0"
+              key={id + i}
+              className={`${styles.cardExperience} ${
+                styles[`experience-${i}`]
+              }`}
+              style={{
+                width: width === '100%' ? width : `calc(${width} - 20px)`,
+              }}>
+              <Atropos className={styles.atroposExperience} {...atroposProps}>
+                <Link
+                  href={
+                    external_link
+                      ? external_link !== ''
+                        ? external_link
+                        : `${process.env.NEXT_PUBLIC_CURR_DOMAIN}${id}`
+                      : `${process.env.NEXT_PUBLIC_CURR_DOMAIN}${id}`
+                  }
+                  passHref={external_link !== null && external_link !== undefined}
+                  target={target}>
                   <div
                     className={`bg-cv ${styles.imgCardExperience}`}
                     style={{
@@ -71,10 +63,10 @@ const ExperienceDetail = ({ contentExp }) => {
                     dangerouslySetInnerHTML={{ __html: title }}
                   />
                 </Link>
-              )}
-            </Atropos>
-          </div>
-        ))}
+              </Atropos>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
