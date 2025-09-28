4c78dc1f4d942b3b5de51340149a392c8b3217b9
Jeyson Rojas <jasson142@gmail.com>
2025-09-28 06:28:26 -0500

    Adding conscius travel page support

---
+++ src/pages/conscious-travel.jsx
@@ -0,0 +1,32 @@
+import Metas from '@/components/metaDatas';
+// import AfterHero from '@/components/after-hero';
+import Hero from '@/components/hero';
+import ConsciousTravel from '@/components/conscious-travel';
+// import ImgSection from '@/components/img-section';
+// import ImgText from '@/components/img-text';
+// import ProudMembers from '@/components/proud-members';
+import React from 'react';
+
+const consciousTravel = ({ data }) => {
+    console.log(data)
+  const { metacontent, hero,  legalcontent } = data;
+
+  return (
+    <>
+      <Metas metadata={metacontent} />
+      <Hero contentHero={hero} />
+      <ConsciousTravel consciousTravel={ legalcontent} />
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const content = await fetch(
+    `${process.env.NEXT_PUBLIC_ENDPOINT_CONTENT}colombian-app/v2/conscious-travel`
+  );
+  const data = await content.json();
+  return {
+    props: { data },
+  };
+}
+export default consciousTravel;
